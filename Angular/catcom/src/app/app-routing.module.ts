2e13d1913101d1f83e3c4b42d7630f431d88149a
CodeDroid99 <johnmwega2002@gmail.com>
2023-02-15 12:37:18 +0300

    new changes

--- Angular/catcom/src/app/app-routing.module.ts
+++ Angular/catcom/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { EventsComponent } from './pages/events/events.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'events',component:EventsComponent},
+  {path:'contacts',component:ContactsComponent},
+  {path:'blog',component:BlogComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
