b7a7a7b474b2b40d439af1381ab94beab252e9b7
CodeDroid99 <johnmwega2002@gmail.com>
2023-02-18 20:35:59 +0300

    installl dependencies

--- fullstack/server/app.js
+++ fullstack/server/app.js
@@ -4,4 +4,12 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/database');
+
+const app ="express";
+const port= 3000;
+
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port '+port);
+  });
