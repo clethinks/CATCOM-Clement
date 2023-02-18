578904f0c256084512d621de9d12edcc923b2f59
CodeDroid99 <johnmwega2002@gmail.com>
2023-02-18 21:22:18 +0300

    add .env file

--- fullstack/app.js
+++ fullstack/app.js
@@ -2,3 +2,7 @@ const express = require('express');
 const createHttpError = require('http-errors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express()
