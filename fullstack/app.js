8d1e3d587bd56acafa6666c4815545829e0bd731
CodeDroid99 <johnmwega2002@gmail.com>
2023-02-18 22:18:55 +0300

    add .env file

--- fullstack/app.js
+++ fullstack/app.js
@@ -5,4 +5,25 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const app = express()
+// Initialization
+const app = express();
+app.use(morgan('dev'));
+
+
+app.get('/', (req,res, next)=>{
+    res.send("Working!")
+});
+
+app.use('/', (req,res, next)=>{
+    next(createHttpError.NotFound())
+});
+
+app.use('/', (error,req,res, next)=>{
+    error.status=error.status || 500;
+    res.status(error.status);
+    res.send("Error");
+});
+const port = process.env.port || 3000;
+
+// Listening for connections on the defined PORT
+app.listen(port, () => console.log(`🚀 started at port:${port}`));
